e66e494200299b0058a42daf30422d4c7d91cbdd
Mateus Aalexandre <j.amadeu2018@gmail.com>
2019-12-13 14:15:36 +0100

    Criação da estrutura de pastas, e api básica

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
+const port = process.env.PORT || 3333;
+
 const app = express();
 
-app.listen(3333, () => {
-  console.log("Server is running");
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello Guys! Divulgue-api"
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running at port 3333");
 });
