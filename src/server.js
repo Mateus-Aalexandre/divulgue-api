c9edad2acee9b8b8a38ad399b9011e14977bec92
Mateus Aalexandre <j.amadeu2018@gmail.com>
2019-12-13 14:19:35 +0100

    alterações no server.js

--- src/server.js
+++ src/server.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello Guys! Divulgue-api"
+    message: "Hello Guys! Divulgue-api..."
   });
 });
 
 app.listen(port, () => {
-  console.log("Server is running at port 3333");
+  console.log(`Server is running at port ${port} `);
 });
